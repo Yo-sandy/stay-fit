1986401580ca0221b5d7852e202ce91f9bda8477
sonu yadav <sonuyadav94220@gmail.com>
2021-04-06 17:10:06 +0530

    stay fit 4/6/21

---
+++ components/front/my-navbar.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #1f1f1f">
+        <div class="container-fluid">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto " >
+              <li class="nav-item">
+                <i class="far fa-clock me-2"></i>
+              </li>
+              <li class="nav-item">
+                 <p  class="gym-time">Time : Monday-Friday (10am - 9pm)</p>
+              </li>
+            </ul>
+            <form class="d-flex">
+             <ul class="m-0 p-0">
+               <li class="social-icon">
+                 <i class="fab fa-facebook-f"></i>
+                 <i class="fab fa-twitter"></i>
+                 <i class="fab fa-instagram"></i>
+               </li>
+             </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #141313">
+        <div class="container">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" style="justify-content: space-between">
+             <div class="stay-fit d-flex">
+               <img src="/stay-fit.jpg" >
+               <h1>STAY</h1>
+               <h1 style="color: var(--iq-primary)">FIT</h1>
+             </div>
+            <div class="d-flex">
+              <ul class="navbar-nav all-link me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <nuxt-link class="nav-link  " to="#">Home</nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link class="nav-link  " to="#">About</nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link class="nav-link  " to="#">Cources</nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link class="nav-link  " to="#">Pages</nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link class="nav-link  " to="#">Features</nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link class="nav-link  " to="#">Blog</nuxt-link>
+                </li>
+                <li class="nav-item">
+                  <nuxt-link class="nav-link  " to="#">Contact Us</nuxt-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "my-navbar"
+}
+</script>
+
+<style scoped>
+.gym-time{
+  color: white;
+  margin: 0;
+}
+.fa-clock{
+  color: white;
+}
+.social-icon i{
+  color: white;
+  font-size: 20px;
+  margin-right: 20px;
+}
+.stay-fit img{
+  max-width: 60px;
+  border-radius: 60%;
+}
+.stay-fit h1{
+  font-weight: bold;
+  color: white;
+  margin-left: 5px;
+}
+.all-link .nav-item a{
+  color: var(--iq-white);
+}
+.all-link .nav-item a:hover{
+  color: var(--iq-primary);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="bg-color">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="shape">
+          <h1>LET'S BUILD<br>YOUR <span style="color: var(--iq-primary)">SHAPE</span></h1>
+        </div>
+        <p style="color:white;padding-top: 30px">Lorem ipsum dolor sit amet, consecteturbr
+          adipisicing elit. A alias Lorem ipsum dolor sit amet, consectetur <br>
+          explicabo id laborum repellendus!
+          consequuntur dolor eligendi harum </p>
+        <button class="btn btn-join" style="background-color: var(--iq-primary);">
+          Join Now
+        </button>
+      </div>
+      <div class="col-md-6">
+        <div class="shape">
+          <img src="/bodybuilding_PNG26.png" class="img-fluid"/>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="brand-logo text-center">
+      <img src="/brand-img/adidas-logo-1.png" alt="">
+      <img src="/brand-img/1200px-Reebok_2019_logo.svg.png" alt="">
+      <img src="/brand-img/nike-png.png" alt="">
+      <img src="/brand-img/puma-png-logo.png" alt="">
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style >
+
+</style>
